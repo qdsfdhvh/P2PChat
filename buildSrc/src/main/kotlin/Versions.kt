9010860da6eaedede1a2f2f9c08616ad1d64515b
Seiko <605590140@qq.com>
2020-06-13 15:58:49 +0800

    build:升级部分依赖。

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -14,18 +14,18 @@ object Versions {
     const val lifecycle = "2.2.0"
 
     const val kotlin = "1.3.72"
-    const val kotlinExtensions = "1.2.0"
-    const val kotlinxCoroutines = "1.3.5"
-    const val kotlinxIo = "0.1.6"
+    const val kotlinExtensions = "1.3.0"
+    const val kotlinxCoroutines = "1.3.7"  // https://github.com/Kotlin/kotlinx.coroutines
+    const val kotlinxIo = "0.1.16"         // https://github.com/Kotlin/kotlinx-io
 
-    const val koin = "2.1.5"
+    const val koin = "2.1.6"   // https://github.com/InsertKoinIO/koin
     const val room = "2.2.5"
     const val navigation = "2.2.2"
     const val work = "2.3.4"
-    const val mmkv = "1.1.1"
-    const val timber = "4.7.1"
-    const val glide = "4.11.0"
-    const val okio = "2.6.0"
+    const val mmkv = "1.1.2"   // https://github.com/Tencent/MMKV
+    const val timber = "4.7.1" // https://github.com/JakeWharton/timber
+    const val glide = "4.11.0" // https://github.com/bumptech/glide
+    const val okio = "2.6.0"   // https://github.com/square/okio
     const val compose = "0.1.0-dev13"
     const val dagger = "2.28-alpha"
     const val hilt = "1.0.0-alpha01"
